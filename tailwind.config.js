99973acfe12cba4da826da49d535292c72e9e858
Rainer Simon <aboutgeo@gmail.com>
2022-07-02 16:35:11 +0200

    Color customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,30 @@ module.exports = {
       sans: ['Nunito', 'sans-serif'],
       serif: ['Nunito', 'serif']
     },
-		extend: {},
+		colors: {
+			'white': '#fff',
+			'steel': {
+				'normal': '#424d83',
+				'light': '#5c69a7',
+				'lighter': '#818ed0'
+			},
+			'lilac': {
+				300: '#d5dbff',
+				500: '#a1affc'
+			},
+			'gray': {
+				50: '#fafafa',
+				100: '#f4f4f4',
+				200: '#e7e7e7',
+				300: '#d5d5d5',
+				400: '#a3a3a3',
+				500: '#727272',
+				600: '#55555',
+				700: '#414141',
+				800: '#292929',
+				900: '#181818'
+			}
+		}
 	},
 	plugins: [],
 };
