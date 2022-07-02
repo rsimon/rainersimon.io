6b0ca519d53bc4758394fdcbcc83f3af52b69ed2
Rainer Simon <aboutgeo@gmail.com>
2022-07-02 15:14:13 +0200

    Starting to build out splash page

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
+	theme: {
+    fontFamily: {
+      sans: ['Nunito', 'sans-serif'],
+      serif: ['Nunito', 'serif']
+    },
+		extend: {},
+	},
+	plugins: [],
+};
