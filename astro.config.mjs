a4ac3a52f5a7d96e57bc31747051753464b8b9a5
Rainer Simon <aboutgeo@gmail.com>
2022-07-03 10:26:31 +0200

    Build

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  outDir: './docs',
 	integrations: [svelte(), tailwind()],
 	vite: {
     ssr: {
