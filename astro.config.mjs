c04531336c0c01acc4e29c8daaa27dc8dac058ca
Rainer Simon <rainer@rainersimon.io>
2024-09-06 20:26:15 +0200

    Migrated to latest Astro + fixed links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
 import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   outDir: './docs',
-	integrations: [svelte(), tailwind()],
+	integrations: [icon(), svelte(), tailwind()],
 	vite: {
     ssr: {
       external: ['svgo']
