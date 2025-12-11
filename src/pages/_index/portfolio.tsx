c894090c19a2bcfe50ca72bcfd0288f2fd6bae32
Rainer Simon <rainer@rainersimon.io>
2025-12-11 08:31:39 +0100

    Anntorious portfolio card

--- src/pages/_index/portfolio.tsx
+++ src/pages/_index/portfolio.tsx
@@ -10,7 +10,7 @@ const PROJECTS = [{
 }, {
   title: 'Annotorious',
   summary: 'Open source JavaScript image annotation library.',
-  image: '/images/portfolio-annotorious.jpg'
+  image: '/assets/img/portfolio-annotorious.jpg'
 }, {
   title: 'Recogito Studio',
   summary: 'Collaborative document annotation for the classroom.',
@@ -28,25 +28,31 @@ const PROJECTS = [{
 export const Portfolio = () => {
 
   return (
-    <div className="grid md:grid-cols-1 lg:grid-cols-2 gap-8">
-      {PROJECTS.map((project, index) => (
-        <div
-          key={index}
-          className="group relative overflow-hidden rounded-lg border border-steel/15 cursor-pointer shadow-lg hover:shadow-2xl transition-all duration-300">
-          <div className="aspect-video relative">
-            <img
-              src={project.image || "/placeholder.svg"}
-              alt={project.title}
-              className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
-            />
+    <div>
+      <h2 className="text-xl font-bold mb-8">
+        Selected Projects
+      </h2>
+    
+      <div className="grid md:grid-cols-1 lg:grid-cols-2 gap-8">
+        {PROJECTS.map((project, index) => (
+          <div
+            key={index}
+            className="group relative overflow-hidden rounded-lg border border-steel/15 cursor-pointer shadow-lg hover:shadow-2xl transition-all duration-300">
+            <div className="aspect-video relative">
+              <img
+                src={project.image || "/placeholder.svg"}
+                alt={project.title}
+                className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
+              />
 
-            <div className="absolute inset-0 bg-linear-to-t from-primary via-primary/95 to-primary/70 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-end p-4">
-              <h3 className="text-xl tracking-wide font-semibold text-primary-foreground">{project.title}</h3>
-              <p className="text-primary-foreground/90">{project.summary}</p>
+              <div className="absolute inset-0 bg-linear-to-t from-primary via-primary/95 to-primary/70 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-end p-4">
+                <h3 className="text-xl tracking-wide font-semibold text-primary-foreground">{project.title}</h3>
+                <p className="text-primary-foreground/90">{project.summary}</p>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   )
 
